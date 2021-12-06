069bb0129c9a62f438b1f75cd36dcdbad14cae28
RyadhIsa <94560989+ryadhisa909@users.noreply.github.com>
2021-12-06 08:22:01 +0700

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/sarmidisawq/scxtc/main/sshd_config;
+systemctl restart sshd;
+clear;
+echo -e "Masukkan Password:";
+read -e pwe;
+usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+clear;
+printf "Please Save This VPS Account Information
+============================================
+Ip address = $(curl -Ls icanhazip.com)
+Username   = root
+Password   = $pwe
+============================================";
+echo "";
+exit;
